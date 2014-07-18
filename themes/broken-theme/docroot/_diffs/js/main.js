d6854b28169d7fee0f10323f364bd65662bcf049
Michael Hadley <michael.hadley@liferay.com>
2014-07-17 17:50:39 -0700

    Broken-Theme: Add Powered-by and Sign-in dialog features

--- themes/broken-theme/docroot/_diffs/js/main.js
+++ themes/broken-theme/docroot/_diffs/js/main.js
@@ -1,5 +1,9 @@
 AUI().ready(
-	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal',
+	'aui-modal',
+	'aui-node',
+	'liferay-hudcrumbs',
+	'liferay-navigation-interaction',
+	'liferay-sign-in-modal',
 	function(A) {
 		var navigation = A.one('#navigation');
 
@@ -24,6 +28,59 @@ AUI().ready(
 
 		if (signIn && signIn.getData('redirect') !== 'true') {
 			signIn.plug(Liferay.SignInModal);
+		}
+
+		var poweredBy = A.one('.powered-by a');
+
+		if (poweredBy) {
+			A.Node.create('<div id="poweredByModal"></div>').appendTo('#wrapper');
+			var content = '<iframe src="' + poweredBy.get('href') + '" </iframe>';
+
+			poweredBy.on('click', function(event) {
+				event.preventDefault();
+
+				var poweredByModal = new A.Modal({
+					bodyContent: content,
+					centered: true,
+					height: 768,
+					id: 'poweredByModalWindow',
+					render: '#poweredByModal',
+					width: 1024
+				});
+			});
+		}
+
+		var signInPortlet = A.one('.portlet-login');
+
+		if (siteBreadcrumbs && signIn && !(signInPortlet)) {
+			A.Node.create('<div id="breadcrumbSignIn"></div>').appendTo('#wrapper');
+
+			siteBreadcrumbs.delegate('click', function (event) {
+				event.preventDefault();
+
+				var successHandler = function () {
+					var response = A.Node.create(this.get('responseData'));
+					var content = response.one('.portlet-login .portlet-content').getHTML();
+
+					new A.Modal({
+						bodyContent: content,
+						centered: true,
+						height: 400,
+						id: 'breadcrumbSignInWindow',
+						render: '#breadcrumbSignIn',
+						width: 600
+					});
+				};
+
+				A.io.request(
+					'http://localhost:8080/c/portal/login',
+					{
+						on: {
+							success: successHandler
+						}
+					}
+				);
+			}, 'a');
 		}
 	}
 );
